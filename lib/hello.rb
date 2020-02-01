e7ce32390c29da3f60a58a5ad67590766176a463
Willis Aybar <willisaybar@gmail.com>
2020-02-01 23:21:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,10 @@
-# def hello_t(array)
-#   i = 0
+def hello_t(array)
+  i = 0
   
-#   while i < array.legnth 
-#     yield array[i]
-#     i = i + 1 
-#   end
-# end
-
-# call your method here!
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+  while i < array.legnth 
+    yield array[i]
+    i = i + 1 
   end
 end
+
+# call your method here!
