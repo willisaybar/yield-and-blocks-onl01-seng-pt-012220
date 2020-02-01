75f2bc74ce58628d5b1ca736d05beb10ec83933d
Willis Aybar <willisaybar@gmail.com>
2020-02-01 23:20:03 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,11 @@
-def hello_t(array)
-  i = 0
+# def hello_t(array)
+#   i = 0
   
-  while i < array.legnth 
-    yield array[i]
-    i = i + 1 
-  end
-end
+#   while i < array.legnth 
+#     yield array[i]
+#     i = i + 1 
+#   end
+# end
 
 # call your method here!
 
